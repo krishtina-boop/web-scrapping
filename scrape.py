01494e521a3a91633ba0836f068222359d885121
krishtina-boop <krishtinamaharjan8@gmail.com>
2025-12-08 13:36:33 +0545

    scraper with data inserted in database

--- scrape.py
+++ scrape.py
@@ -1,9 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import csv
+import sqlite3
 url="https://books.toscrape.com/"
+con = sqlite3.connect('students1.sqlite3')
+cur = con.cursor()
 
-def scrape_books(url):
+def create_table():
+    book_query="""
+    create table book(
+    id integer primary key autoincrement,
+    title varchar not null,
+    currency varchar not null,
+    price varchar not null
+    )
+    """
+    cur.execute(book_query)
+    con.commit()
+
+def insert_items(title, currency, price):
+    insertion = """
+    insert into book('title','currency','price') values (?,?,?)
+    """
+    
+    cur.execute(insertion,(title, currency, price))
+    con.commit()
+    
+def read_items():
+    select = "select * from book"
+    cur.execute(select)
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+def scarpe_books(url):
     response = requests.get(url)
     if(response.status_code != 200):
         print(f"failed to fetch!Status code {response.status_code}")
@@ -35,14 +68,28 @@ def scrape_books(url):
         }
         all_books.append(book_data)
         
-        json_data = json.dumps(all_books, indent=4)
-        print(json_data)
-        
-        # print(all_books)
-        
-        with open("books.json","w") as f:
-            json.dump(all_books, f, indent=4)
+    return all_books
 
 
-   
-scrape_books(url)
+def json_file(all_books):
+    with open("books.json","w",encoding="utf-8") as f:
+        json.dump(all_books, f, indent=4, ensure_ascii=False)
+
+def csv_file(all_books):
+    headers = all_books[0].keys()
+    with open("books.csv","w",newline="",encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=all_books[0].keys())
+        writer.writeheader()
+        writer.writerows(all_books) 
+
+def main():
+    # create_table()
+    books = scarpe_books(url)
+    for b in books:
+        insert_items(b['title'], b['currency'], b['price'])
+    
+    json_file(books)
+    csv_file(books)
+    read_items()
+
+main()
